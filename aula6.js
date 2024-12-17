19811859fbb096bfb1dfefe7b622649c0d9d1e90
Leonardo Arruda Ferreira <leoarruda2007@gmail.com>
2024-12-17 14:25:41 -0300

    Update aula6.js

--- aula6.js
+++ aula6.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data= fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 const server= http.createServer((req,res) => {
     const pathName = req.url;
     if(pathName === '/' || pathName ==='/overview'){
@@ -9,11 +11,8 @@ const server= http.createServer((req,res) => {
     }else if (pathName ==='/product'){
         res.end('This is PRODUCT')
     }else if(pathName === '/api'){
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err,data) =>{
-         const productData = JSON.parse(data);
-         res.writeHead(200, {'Content_type' :'application/json'})
-         res.end(data)
-    })
+         res.writeHead(200, {'Content_type' :'application/json'});
+         res.end(data);
     }else {
         res.writeHead(404,{
             'Content-type':'text/html',
