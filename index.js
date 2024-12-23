77fdb67f4a870b4fb8ac014ca1dc65efc26b6a2d
Leonardo Arruda Ferreira <leoarruda2007@gmail.com>
2024-12-23 16:01:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ const server= http.createServer((req,res) => {
 
     
 });
-server.listen(8000, '127.0.0.1', ()=> {
+server.listen(8000, '127.0.0.0', ()=> {
     console.log('Listening to request on port 8000')
 });
