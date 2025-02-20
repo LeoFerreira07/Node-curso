952dd2f3978ed1c51132e5670aa951021ad9e550
Leonardo Arruda Ferreira <147511241+LeoFerreira07@users.noreply.github.com>
2025-02-20 17:59:25 -0300

    Update aula6.js

--- Server/aula6.js
+++ Server/aula6.js
@@ -2,27 +2,27 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const data= fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-const server= http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
     const pathName = req.url;
-    if(pathName === '/' || pathName ==='/overview'){
+    if (pathName === '/' || pathName === '/overview') {
         res.end('This is OVERVIEW')
-    }else if (pathName ==='/product'){
+    } else if (pathName === '/product') {
         res.end('This is PRODUCT')
-    }else if(pathName === '/api'){
-         res.writeHead(200, {'Content_type' :'application/json'});
-         res.end(data);
-    }else {
-        res.writeHead(404,{
-            'Content-type':'text/html',
-            'My-own-header':'hello-wordl'
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content_type': 'application/json' });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'My-own-header': 'hello-wordl'
         });
         res.end('<h1> Page not found!<h1>');
     }
 
-    
+
 });
-server.listen(8000, '127.0.0.1', ()=> {
+server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to request on port 8000')
 });
